8976e4e50dec68958672b3e28db4297d396f8cd7
Valdiberto <junior-dk555@hotmail.com>
2025-04-16 10:36:58 -0300

    viteconfig

--- packages/docs/vite.config.ts
+++ packages/docs/vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
+  base: '/05-design-system/', // Configuração do caminho base no Vite
   plugins: [react()],
 })
